7299324e6ab0633347abb94a6d10589d675e62f0
Bernadette McKnight <sissyhanks@yahoo.com>
2021-12-08 11:55:53 -0500

    pseudo code calcAverage function

--- challenge.js
+++ challenge.js
@@ -36,7 +36,7 @@ for (let i = 0; i < bills.length; i++) {
 console.log(tips);
 console.log(totals);
 
-//for loop declares total variable at value 0, adds all number values from an array to the total value, and then divides the total value by the number of number values in the array
+//function 'calcAverage' takes in array 'arr' and runs for loop that declares 'total' variable at value 0, adds all number values from 'arr' to the 'total' value, and then divides the 'total' value by the number of number values in 'arr'
 
 
 
