de076d160246d2369ae382dd1421c660d4e6b777
Bernadette McKnight <sissyhanks@yahoo.com>
2021-12-27 14:03:21 -0500

    return max and min temps

--- starter/script.js
+++ starter/script.js
@@ -9,7 +9,7 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // array of only numbers
 let temps = [];
-for (let i = 0; i < temperatures.length; ++i) {
+for (let i = 0; i < temperatures.length; i++) {
   if (typeof temperatures[i] === 'number') {
     temps.push(temperatures[i]);
   }
@@ -19,5 +19,26 @@ console.log(temps);
 
 //a function that takes in the array of temperature data and returns difference between highest and lowest temps
 const tempAmp = function (temps) {
+  // value of max temp set to first temp in temps array
+  let max = temps[0];
+  // starting at the second item in temps array, loop through logic through to the end of array
+  for (let i = 0; i < temps.length; i++) {
+    //compare number in array to number held in max, if array item greater update that number to value held in max
+    if (temps[i] > max) {
+      max = temps[i];
+    }
+  }
+    // value of min temp set to first temp in temps array
+  let min = temps[0];
+  // starting at the second item in temps array, loop through logic through to the end of array
+  for (let i = 0; i < temps.length; i++) {
+    //compare number in array to number held in max, if array item less, update that number to value held in max
+    if (temps[i] < min) {
+      min = temps[i];
+    }
+  }
+  console.log(max, min);
 
 }
+
+tempAmp(temps);
