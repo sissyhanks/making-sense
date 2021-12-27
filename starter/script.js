58d7cd3dc18948f29767f4ae163b908ec4a6ad8c
Bernadette McKnight <sissyhanks@yahoo.com>
2021-12-27 13:15:15 -0500

    set up branch

---
+++ starter/script.js
@@ -0,0 +1,8 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// problem
+// smart home thermomotor company wants a way to calculate daily temp amplitude
+// some reading will be an error
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
