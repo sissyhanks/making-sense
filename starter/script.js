669ccfff9c763305a875bc2b1193e1c929406fbf
Bernadette McKnight <sissyhanks@yahoo.com>
2021-12-27 13:52:37 -0500

    return array of only numbers

--- starter/script.js
+++ starter/script.js
@@ -5,4 +5,19 @@
 // smart home thermomotor company wants a way to calculate daily temp amplitude
 // some reading will be an error
 
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// array of only numbers
+let temps = [];
+for (let i = 0; i < temperatures.length; ++i) {
+  if (typeof temperatures[i] === 'number') {
+    temps.push(temperatures[i]);
+  }
+}
+
+console.log(temps);
+
+//a function that takes in the array of temperature data and returns difference between highest and lowest temps
+const tempAmp = function (temps) {
+
+}
