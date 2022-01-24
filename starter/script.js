c87236d87e659227f8fef13d53f6d9fca3ea439e
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-24 12:32:33 -0500

    score handling

--- starter/script.js
+++ starter/script.js
@@ -17,6 +17,8 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 //event listeners need type of event and function to tell what to do 'event handler' is expected and the function value gets passed in >> the function passed in is only called on teh event happening
 
 //so in the function passed into the event listener create a constant that holds the value entered in element with class of guess >> value will return as string so convert to number for later comparison
@@ -32,8 +34,12 @@ document.querySelector('.check').addEventListener('click', function () {
       '.message'
     ).textContent = `Yes! The secret number is ${secretNumber}. You win!`;
   } else if (guess > secretNumber) {
+    score--;
+    document.querySelector('.score').textContent = score;
     document.querySelector('.message').textContent = `too high. guess again`;
   } else if (guess < secretNumber) {
+    score--;
+    document.querySelector('.score').textContent = score;
     document.querySelector('.message').textContent = `Too Low. Guess again.`;
   }
 });
