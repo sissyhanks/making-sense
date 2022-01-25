61a426bcebedef0b901224943aaa2b735a1a27e2
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-25 09:56:45 -0500

    win handling

--- starter/script.js
+++ starter/script.js
@@ -1,23 +1,10 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent);
 
-// select the text of the element with class of message and set it equal to new string to change text
-// document.querySelector('.message').textContent = 'Correct Number';
-
-// document.querySelector('.number').textContent = '55';
-
-// document.querySelector('.score').textContent = '45';
-
-// console.log(document.querySelector('.guess').value);
-
-// document.querySelector('.guess').value = 44;
-
-// console.log(document.querySelector('.guess').value);
-
+//set opening score value
 let score = 20;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-document.querySelector('.number').textContent = secretNumber;
+// create secret number to be guessed
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 function newScore() {
   if (score > 0) {
@@ -36,15 +23,23 @@ function loss() {
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  //error handling if no guess
   if (!guess || guess > 20) {
+    //error handling if no guess
     document.querySelector('.message').textContent =
       'Please enter a number between 1 and 20!';
   } else if (guess === secretNumber) {
+    //winning guess
+    // change body background to green
+    // display secretNumber in and widen .number box
+    // send winning message to .textContent
     document.querySelector(
       '.message'
     ).textContent = `Yes! The secret number is ${secretNumber}. You win!`;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
+    //too high
     newScore();
     if (score > 0) {
       document.querySelector('.message').textContent = `Too high. Guess again.`;
@@ -52,6 +47,7 @@ document.querySelector('.check').addEventListener('click', function () {
       loss();
     }
   } else if (guess < secretNumber) {
+    //too low
     newScore();
     if (score > 0) {
       document.querySelector('.message').textContent = `Too Low. Guess again.`;
