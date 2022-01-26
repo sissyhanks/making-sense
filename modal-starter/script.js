97a6c4fdd19af2d17b0f3ab4a936dac595d2002a
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-26 14:16:52 -0500

    create named open and close functions

--- modal-starter/script.js
+++ modal-starter/script.js
@@ -7,16 +7,22 @@ const closeBtn = document.querySelector('.close-modal');
 //the elements can then be looped through like an array using the variable name for the elements selected by class as the 'array' name
 const showBtns = document.querySelectorAll('.show-modal');
 
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
 console.log(showBtns);
 
 for (let i = 0; i < showBtns.length; i++)
-  showBtns[i].addEventListener('click', function () {
-    console.log(`button ${showBtns[i].textContent} has been clicked`);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  showBtns[i].addEventListener('click', openModal);
 
-closeBtn.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+//when inserting a named function as an argument in click event don't 'call' it with () or it will exicute immediatly
+closeBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
