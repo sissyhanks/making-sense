b62e2d154c81537dab144bc1ca3daba8a501dfca
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-26 14:05:07 -0500

    show modal functionality

--- modal-starter/script.js
+++ modal-starter/script.js
@@ -3,4 +3,19 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-modal');
-const showBtns = document.querySelector('.show-modal');
+//to select all elements of a class name use querySelectorAll
+//the elements can then be looped through like an array using the variable name for the elements selected by class as the 'array' name
+const showBtns = document.querySelectorAll('.show-modal');
+
+console.log(showBtns);
+
+for (let i = 0; i < showBtns.length; i++)
+  showBtns[i].addEventListener('click', function () {
+    console.log(`button ${showBtns[i].textContent} has been clicked`);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+// closeBtn.addEventListener('click', function (){
+//   modal.classList.
+// })
