a9d967bbb4301188d55a6e3a220437358fa51ace
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-26 13:29:45 -0500

    create element variables

--- modal-starter/script.js
+++ modal-starter/script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const showBtns = document.querySelector('.show-modal');
