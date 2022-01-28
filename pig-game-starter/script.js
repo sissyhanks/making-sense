6da87a710c3410a394c929ba1ef76367f4c12024
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-28 14:06:20 -0500

    switch active player

--- pig-game-starter/script.js
+++ pig-game-starter/script.js
@@ -1,37 +1,52 @@
 'use strict';
 
 //selecting elements
+const playerZeroEl = document.querySelector('.player--0');
+const playerOneEl = document.querySelector('.player--1');
 const scoreElZero = document.querySelector('#score--0');
 const scoreElOne = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn--roll');
 const newBtn = document.querySelector('.btn--new');
 const holdBtn = document.querySelector('.btn--hold');
-// const scoreZero = document.querySelector('#score--0');
-// const scoreOne = document.querySelector('#score--1');
-// const currentScoreZero = document.querySelector('#current--0');
-// const currentScoreOne = document.querySelector('#current--0');
+const currentScoreZero = document.querySelector('#current--0');
+const currentScoreOne = document.querySelector('#current--1');
 
 //variables
 // number rolled
-let numberRolled;
-//context for which player is active
-let currentPlayer = true;
+let dice;
 
 scoreElOne.textContent = 0;
 scoreElZero.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+//context for which player is active
+let activePlayer = 0;
+
+let score0 = 0;
+let score1 = 0;
 
 function rollNumber() {}
 
 rollBtn.addEventListener('click', function () {
-  numberRolled = Math.trunc(Math.random() * 6) + 1;
-  diceEl.src = `dice-${numberRolled}.png`;
+  dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.src = `dice-${dice}.png`;
   diceEl.classList.remove('hidden');
-  if (numberRolled !== 1) {
-    currentScore += numberRolled;
-    console.log(currentScore);
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    playerZeroEl.classList.toggle('player--active');
+    playerOneEl.classList.toggle('player--active');
   }
 });
+
+// holdBtn.addEventListener('click', function () {
+//   score${activePlayer}
+// });
