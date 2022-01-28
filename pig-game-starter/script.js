571df668e7832a8c91558bad3740a6b8cf66040e
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-28 10:16:36 -0500

    create innitial game settings

--- pig-game-starter/script.js
+++ pig-game-starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//selecting elements
+const scoreElZero = document.querySelector('#score--0');
+const scoreElOne = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+scoreElOne.textContent = 0;
+scoreElZero.textContent = 0;
+diceEl.classList.add('hidden');
