f3d6d3165a5bdc26bbf15487b90f5fb39f541005
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-28 12:50:28 -0500

    create random roll that changes dice display

--- pig-game-starter/script.js
+++ pig-game-starter/script.js
@@ -8,3 +8,9 @@ const diceEl = document.querySelector('.dice');
 scoreElOne.textContent = 0;
 scoreElZero.textContent = 0;
 diceEl.classList.add('hidden');
+
+function rollNumber() {
+  let number = Math.trunc(Math.random() * 6) + 1;
+  diceEl.src = `dice-${number}.png`;
+  diceEl.classList.remove('hidden');
+}
