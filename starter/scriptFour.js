a258159f91a24f473aeb58b73664d2fe30a5d4f7
Bernadette McKnight <sissyhanks@yahoo.com>
2022-02-19 14:26:04 -0500

    working with strings 2

--- starter/scriptFour.js
+++ starter/scriptFour.js
@@ -36,3 +36,62 @@ const checkMiddleSeat = function (seat) {
 checkMiddleSeat('11B');
 checkMiddleSeat('23A');
 checkMiddleSeat('4E');
+
+// ---WORKING WITH STRINGS 2---
+//transform sdchrachters toi upper or lowecase
+console.log(arline.toLowerCase());
+console.log(arline.toUpperCase());
+
+const passenger = 'bErNiE';
+//to make 1st upper only first convert all to lower case in new varaible
+const passengerLower = passenger.toLowerCase();
+console.log(passengerLower.slice(1));
+// next take first character and upper it (dont forget to call ()) & add the rest of the lower name sliced at 1 index
+const passengerFix = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+
+console.log(passengerFix);
+
+//use case, compare email
+const email = 'bernie@email.com';
+const loginEmail = '  BerNie@emAIL.cOm \n';
+//first step convert entry to lower case
+lowerEmail = loginEmail.toLowerCase();
+//remove whitespace
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+//becuse method returns a string can chain methods can do both in one step
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+//replacing >> replace works on first occurrence
+const priceGB = '540,77£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'Al passengers come to boarding door 23. Boarding door 23.';
+//replaceAll is a thing!!
+console.log(announcement.replaceAll('door', 'gate'));
+//or use regular expression >> write thing to be replaced between slashes with g following to indicate global
+console.log(announcement.replace(/door/g, 'gate'));
+//string methods that return booleans
+const aPlane = 'A320neo';
+console.log(aPlane.includes('A320'));
+console.log(aPlane.startsWith('Air'));
+console.log(aPlane.endsWith('eo'));
+
+//practice exercise
+const checkBaggage = function (items) {
+  //set to lower case
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('you may not check your bag');
+  } else {
+    console.log('you are ready to go!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket knife');
+checkBaggage('i have socks and Shoes');
+checkBaggage('Got some chips and a gun for protections');
