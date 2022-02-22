92df733cd34c9682c03292ef6db7f79de0393c70
Bernadette McKnight <sissyhanks@yahoo.com>
2022-02-22 08:54:58 -0500

    immediately invoked function expressions

--- A-Closer-Look-at-Functions/scriptTwo.js
+++ A-Closer-Look-at-Functions/scriptTwo.js
@@ -1,1 +1,16 @@
 'use strict';
+//--IMMEDIATELY INVOKED FUNCTION EXPRESSIONS--
+//when you need a function once & then want it to disappear (will help with async / await)
+const runOnce = function () {
+  console.log('this will run again');
+};
+runOnce();
+//write function... not assigning it io a variable ... wrap in parenthasies to change statement into expression... and then call it immediatly after wrap
+(function () {
+  console.log('this will never run again');
+})();
+
+// as an arrow >> call to arrow to run, wrap in parentheses and cal immediately
+(() => console.log('one time arrow'))();
+
+//need this because of function scope, when we don't want variables to be overridden, IIFE used to ';hide' varaibles, keep them private
