e822ef146acd6c4920650080a1c7c203d1dfc36c
Bernadette McKnight <sissyhanks@yahoo.com>
2022-02-24 10:05:02 -0500

    array methods

--- working-with-arrays/scriptOne.js
+++ working-with-arrays/scriptOne.js
@@ -1,5 +1,6 @@
 'use strict';
-
+//methods are functions we can call on objects... they are attached to objects
+// since arrays have methods that means they are objects & have methods attached
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
@@ -74,3 +75,25 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+//--SLICE-- returns a new array with only extracted parts takes two parameters, the index of teh first item you want returned & the index after the last item you want returned :: or leave second parameter blank to return to the end of the array :: the length of output wil be end parameter minus beginning parameter :: enter a negative parameter to start return from end of array, wil return as many items as number of parameter (-1 is always last number of array) >> can also put negative parameter as second parameter to count back from last item in array :: to make array copy of array, can use slice with no parameter >> can chain functions after slice copy (as apposed to spread copy)
+
+console.log(arr.slice(2));
+
+//--SPLICE-- splice will mutate original array ... taking out any
+const arrrr = arr.splice(1, 2);
+console.log(arr);
+console.log(arrrr);
+
+//--REVERSE-- reverses array >> mutates original so original holds reversed !
+const arr2 = ['q', 'p', 'o', 'n', 'm'];
+arr2.reverse();
+console.log(arr2);
+
+//--CONCAT-- makes new array of 2 arrays ... could also do [spread, spread] and get new array of 2 arrays
+const letters = arr.concat(arr2);
+console.log(letters);
+
+//--JOIN-- will join elements of an array into a string separated with characters specified inside parameter call
+console.log(letters.join(' '));
