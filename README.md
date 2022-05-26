aa10605363518c52b3cd134346df4ec16530e318
Bernadette McKnight <sissyhanks@yahoo.com>
2022-05-26 12:18:32 -0400

    map notes

--- README.md
+++ README.md
@@ -115,6 +115,10 @@ these methods create new arrays, transforming data from other arrays
 
 ### The map Method
 
+- map is a more functional approach than using a for each loop
+
+- declare a variable and set it equal to an instance of the map method and the variable will be set to an array (the new array transformed by the parameters in the function)
+
 ### Computing Usernames
 
 ### The filter Method
--- working-with-arrays/scriptTwo.js
+++ working-with-arrays/scriptTwo.js
@@ -61,6 +61,26 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,7 +94,7 @@ const currencies = new Map([
 //movements in euros
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+/////////////////////////////////////////////////MAP
 
 //if one euro - 1.1 dollar
 const euroToUsd = 1.1;
